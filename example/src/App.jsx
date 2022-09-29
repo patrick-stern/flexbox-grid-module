e3980ea47c54d67a3a966840741063e05b13575c
Patrick Stern <patrick.stern@valid-digital.com>
2022-09-29 23:53:46 +0200

    [UPDATE] styles // example

--- example/src/App.jsx
+++ example/src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import { Container, Col, Row } from '@strn_dev/flexbox-grid-module';
-// import { Container, Row, Col } from '../../src/';
 import '@strn_dev/flexbox-grid-module/dist/index.css';
 
 
 function App() {
   return (
     <div>
-        <Container className="containerPlus">
+        <Container>
             <Row justifyContent="space-between">
                 <Col sm={12} md={6} lg={3}>
                     <div className='item'></div>
