9598ccd7a8724f5366ab9c345ccea7a174c5230f
Patrick Stern <patrick.stern@valid-digital.com>
2022-09-29 20:54:38 +0200

    Initial commit

---
+++ example/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import './App.css'
+import { Container, Col, Row } from '@strn_dev/flexbox-grid-module';
+// import { Container, Row, Col } from '../../src/';
+import '@patrick_s/flexbox-grid-module/dist/index.css';
+
+
+function App() {
+  return (
+    <div>
+        <Container className="containerPlus">
+            <Row justifyContent="space-between">
+                <Col sm={12} md={6} lg={3}>
+                    <div className='item'></div>
+                </Col>
+                <Col sm={12} md={6} lg={3}>
+                    <div className='item'></div>
+                </Col>
+                <Col sm={12} md={6} lg={3}>
+                    <div className='item'></div>
+                </Col>
+            </Row>
+         
+        </Container>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,3 @@
+export { default as Container } from './components/Container';
+export { default as Row } from './components/Row';
+export { default as Col } from './components/Col';
